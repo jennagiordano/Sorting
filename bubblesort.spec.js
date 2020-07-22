c92a7aa497da3da4dc9c5133ef7dfd2d9d4f4437
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-07-22 12:57:16 -0400

    bubblesort completed

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,9 +1,17 @@
 describe('Bubble Sort', function(){
+  beforeAll(function () {
+    spyOn(sorter, 'swap').and.callThrough()
+  })
   it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+    expect( sorter.bubbleSort([]) ).toEqual( [] );
   });
   it('handles arrays of multiple items', function(){
-    expect(bubbleSort([1,5,6,3,7,8,2,9,4])).toEqual([1,2,3,4,5,6,7,8,9])
+    expect(sorter.bubbleSort([1,5,6,3,7,8,2,9,4])).toEqual([1,2,3,4,5,6,7,8,9]),
+  });
+  it('should call swap for each element * array.length', function () {
+    console.log('this >> ', this)
+   sorter.bubbleSort([1,5,6,3,7,8,2,9,4])
+    expect(sorter.swap.calls.count()).toEqual(24)
   })
 });
 
